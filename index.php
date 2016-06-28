6b57c734f779ea5cfe2aecefaa22a3f56e82e988
dexter0us <uetian.dawer@gmail.com>
2016-06-29 01:37:05 +0500

    added the text file

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 <?php
-// the car GSM application.
-$x = 0;
-echo "Lets Try Heroku.";
+// the car GPRS application.
+echo "Car Wash GPRS Server Application.";
+//Actual Code Starts here:
+//==========================================================
+$value;
 
+$file1="time.txt";
+$myfile = fopen($file1, "a") or die("Unable to open file!");
+
+
+
+
+if (isset($value))
+	fwrite($myfile,$value);
+fclose($myfile);
+//-----------------------------------------------------------
 ?>
