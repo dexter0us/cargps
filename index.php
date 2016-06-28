8de72c46018883d5de46f47911f99512ecfbe778
dexter0us <uetian.dawer@gmail.com>
2016-06-29 02:00:01 +0500

    added some file write things...

--- index.php
+++ index.php
@@ -3,16 +3,22 @@
 echo "Car Wash GPRS Server Application.";
 //Actual Code Starts here:
 //==========================================================
-$value;
+$arr1 =  array();
+$arr1 =  json_decode(file_get_contents('php://input'), true);
+
+$dev_id = $arr1['id'];
+$w_con=$arr1['wc'];
+$s_con=$arr1['sc'];
+$u_num=$arr1['num'];
 
 $file1="time.txt";
 $myfile = fopen($file1, "a") or die("Unable to open file!");
 
-
-
-
-if (isset($value))
-	fwrite($myfile,$value);
+if (isset($dev_id)  && isset($w_con) && isset($s_con) && isset($num))
+	{
+		$value = "Device ID = " . $dev_id . ", Water Consumption = ". $w_con . ", Soap Consumption = " . $s_con . ", U_number = " . $num;
+		fwrite($myfile,$value);
+	}
 fclose($myfile);
 //-----------------------------------------------------------
 ?>
